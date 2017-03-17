2196735fa1244773c02841f6f41149e16fad8760
荣刚 <363642022@qq.com>
2017-03-18 00:08:31 +0800

    Initial Commit

---
+++ FitnessECartoon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FitnessECartoon
+//
+//  Created by 荣刚 on 17/3/18.
+//  Copyright © 2017年 荣刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FitnessECartoon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FitnessECartoon
+//
+//  Created by 荣刚 on 17/3/18.
+//  Copyright © 2017年 荣刚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
